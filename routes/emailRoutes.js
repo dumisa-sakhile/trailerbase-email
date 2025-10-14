eaf2e680b3df018131a5d6dcc102da2d2775910e
dumisa-sakhile <contact.dumisa@gmail.com>
2025-10-14 12:11:02 +0200

    update email template

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -37,196 +37,66 @@ const createEmailRouter = (resend) => {
         to,
         subject: "Welcome to Trailerbase!",
           html: `
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
-<head>
-<title></title>
-<meta charset="UTF-8" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<!--[if !mso]>-->
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<!--<![endif]-->
-<meta name="x-apple-disable-message-reformatting" content="" />
-<meta content="target-densitydpi=device-dpi" name="viewport" />
-<meta content="true" name="HandheldFriendly" />
-<meta content="width=device-width" name="viewport" />
-<meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no" />
-<style type="text/css">
-table {
-border-collapse: separate;
-table-layout: fixed;
-mso-table-lspace: 0pt;
-mso-table-rspace: 0pt
-}
-table td {
-border-collapse: collapse
-}
-.ExternalClass {
-width: 100%
-}
-.ExternalClass,
-.ExternalClass p,
-.ExternalClass span,
-.ExternalClass font,
-.ExternalClass td,
-.ExternalClass div {
-line-height: 100%
-}
-body, a, li, p, h1, h2, h3 {
--ms-text-size-adjust: 100%;
--webkit-text-size-adjust: 100%;
-}
-html {
--webkit-text-size-adjust: none !important
-}
-body {
-min-width: 100%;
-Margin: 0px;
-padding: 0px;
-}
-body, #innerTable {
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale
-}
-#innerTable img+div {
-display: none;
-display: none !important
-}
-img {
-Margin: 0;
-padding: 0;
--ms-interpolation-mode: bicubic
-}
-h1, h2, h3, p, a {
-line-height: inherit;
-overflow-wrap: normal;
-white-space: normal;
-word-break: break-word
-}
-a {
-text-decoration: none
-}
-h1, h2, h3, p {
-min-width: 100%!important;
-width: 100%!important;
-max-width: 100%!important;
-display: inline-block!important;
-border: 0;
-padding: 0;
-margin: 0
-}
-a[x-apple-data-detectors] {
-color: inherit !important;
-text-decoration: none !important;
-font-size: inherit !important;
-font-family: inherit !important;
-font-weight: inherit !important;
-line-height: inherit !important
-}
-u + #body a {
-color: inherit;
-text-decoration: none;
-font-size: inherit;
-font-family: inherit;
-font-weight: inherit;
-line-height: inherit;
-}
-a[href^="mailto"],
-a[href^="tel"],
-a[href^="sms"] {
-color: inherit;
-text-decoration: none
-}
-</style>
-<style type="text/css">
-@media (min-width: 481px) {
-.hd { display: none!important }
-}
-</style>
-<style type="text/css">
-@media (max-width: 480px) {
-.hm { display: none!important }
-}
-</style>
-<style type="text/css">
-@media (max-width: 480px) {
-.t46{padding:0 0 22px!important}.t31,.t42,.t67{text-align:center!important}.t30,.t41,.t66{vertical-align:top!important;width:600px!important}.t28{border-bottom-right-radius:0!important;border-bottom-left-radius:0!important;padding:30px!important}.t75{mso-line-height-alt:20px!important;line-height:20px!important}
-}
-</style>
-<!--[if !mso]>-->
-<link href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@500;800;900&amp;display=swap" rel="stylesheet" type="text/css" />
-<!--<![endif]-->
-<!--[if mso]>
-<xml>
-<o:OfficeDocumentSettings>
-<o:AllowPNG/>
-<o:PixelsPerInch>96</o:PixelsPerInch>
-</o:OfficeDocumentSettings>
-</xml>
-<![endif]-->
-</head>
-<body id="body" class="t78" style="min-width:100%;Margin:0px;padding:0px;background-color:#E0E0E0;"><div class="t77" style="background-color:#E0E0E0;"><table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0" align="center"><tr><td class="t76" style="font-size:0;line-height:0;mso-line-height-rule:exactly;background-color:#E0E0E0;" valign="top" align="center">
-<!--[if mso]>
-<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false">
-<v:fill color="#E0E0E0"/>
-</v:background>
-<![endif]-->
-<table role="presentation" width="100%" cellpadding="0" cellspacing="0" border="0" align="center" id="innerTable"><tr><td align="center">
-<table class="t49" role="presentation" cellpadding="0" cellspacing="0" style="Margin-left:auto;Margin-right:auto;"><tr><td width="566" class="t48" style="width:566px;">
-<table class="t47" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t46" style="padding:50px 10px 31px 10px;"><div class="t45" style="width:100%;text-align:center;"><div class="t44" style="display:inline-block;"><table class="t43" role="presentation" cellpadding="0" cellspacing="0" align="center" valign="top">
-<tr class="t42"><td></td><td class="t41" width="546" valign="top">
-<table role="presentation" width="100%" cellpadding="0" cellspacing="0" class="t40" style="width:100%;"><tr><td class="t39" style="background-color:transparent;"><table role="presentation" width="100%" cellpadding="0" cellspacing="0" style="width:100% !important;"><tr><td align="center">
-<table class="t38" role="presentation" cellpadding="0" cellspacing="0" style="Margin-left:auto;Margin-right:auto;"><tr><td width="546" class="t37" style="width:600px;">
-<table class="t36" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t35"><div class="t34" style="width:100%;text-align:center;"><div class="t33" style="display:inline-block;"><table class="t32" role="presentation" cellpadding="0" cellspacing="0" align="center" valign="top">
-<tr class="t31"><td></td><td class="t30" width="546" valign="top">
-<table role="presentation" width="100%" cellpadding="0" cellspacing="0" class="t29" style="width:100%;"><tr><td class="t28" style="overflow:hidden;background-color:#F8F8F8;padding:40px 50px 40px 50px;border-radius:0 0 18px 18px;"><table role="presentation" width="100%" cellpadding="0" cellspacing="0" style="width:100% !important;"><tr><td align="left">
-<table class="t4" role="presentation" cellpadding="0" cellspacing="0" style="Margin-right:auto;"><tr><td width="381" class="t3" style="width:381px;">
-<table class="t2" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t1"><h1 class="t0" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:41px;font-weight:800;font-style:normal;font-size:30px;text-decoration:none;text-transform:none;letter-spacing:-1.56px;direction:ltr;color:#415AE8;text-align:left;mso-line-height-rule:exactly;mso-text-raise:3px;">Welcome to TrailerBase</h1></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td><div class="t5" style="mso-line-height-rule:exactly;mso-line-height-alt:25px;line-height:25px;font-size:1px;display:block;">&nbsp;&nbsp;</div></td></tr><tr><td align="left">
-<table class="t10" role="presentation" cellpadding="0" cellspacing="0" style="Margin-right:auto;"><tr><td width="446" class="t9" style="width:563px;">
-<table class="t8" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t7"><p class="t6" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:22px;font-weight:500;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;letter-spacing:-0.56px;direction:ltr;color:#333333;text-align:left;mso-line-height-rule:exactly;mso-text-raise:2px;">Welcome to TrailerBase ${userName}! We are pleased to have you.</p></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td><div class="t11" style="mso-line-height-rule:exactly;mso-line-height-alt:15px;line-height:15px;font-size:1px;display:block;">&nbsp;&nbsp;</div></td></tr><tr><td align="left">
-<table class="t16" role="presentation" cellpadding="0" cellspacing="0" style="Margin-right:auto;"><tr><td width="268" class="t15" style="width:268px;">
-<table class="t14" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t13" style="overflow:hidden;background-color:#415AE8;text-align:center;line-height:44px;mso-line-height-rule:exactly;mso-text-raise:10px;padding:0 30px 0 30px;border-radius:40px 40px 40px 40px;"><a class="t12" href="https://www.trailerbase.tech/" style="display:block;margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:44px;font-weight:800;font-style:normal;font-size:12px;text-decoration:none;text-transform:uppercase;letter-spacing:2.4px;direction:ltr;color:#FFFFFF;text-align:center;mso-line-height-rule:exactly;mso-text-raise:10px;" target="_blank">Start bookmarking</a></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td><div class="t17" style="mso-line-height-rule:exactly;mso-line-height-alt:15px;line-height:15px;font-size:1px;display:block;">&nbsp;&nbsp;</div></td></tr><tr><td align="left">
-<table class="t22" role="presentation" cellpadding="0" cellspacing="0" style="Margin-right:auto;"><tr><td width="446" class="t21" style="width:563px;">
-<table class="t20" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t19"><p class="t18" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:22px;font-weight:500;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;letter-spacing:-0.56px;direction:ltr;color:#333333;text-align:left;mso-line-height-rule:exactly;mso-text-raise:2px;">Trailer Base is a movie and TV trailer platform that provides users with the latest trailers and information about their favorite movies and TV shows.</p></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td align="left">
-<table class="t27" role="presentation" cellpadding="0" cellspacing="0" style="Margin-right:auto;"><tr><td width="446" class="t26" style="width:563px;">
-<table class="t25" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t24"><p class="t23" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:22px;font-weight:500;font-style:normal;font-size:14px;text-decoration:none;text-transform:none;letter-spacing:-0.56px;direction:ltr;color:#333333;text-align:left;mso-line-height-rule:exactly;mso-text-raise:2px;">Now that you have created an account, when you login, you will have an ability to bookmark any movie or series you like. Enjoy and start bookmarking!</p></td></tr></table>
-</td></tr></table>
-</td></tr></table></td></tr></table>
-</td>
-<td></td></tr>
-</table></div></div></td></tr></table>
-</td></tr></table>
-</td></tr></table></td></tr></table>
-</td>
-<td></td></tr>
-</table></div></div></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td align="center">
-<table class="t74" role="presentation" cellpadding="0" cellspacing="0" style="Margin-left:auto;Margin-right:auto;"><tr><td width="600" class="t73" style="width:600px;">
-<table class="t72" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t71"><div class="t70" style="width:100%;text-align:center;"><div class="t69" style="display:inline-block;"><table class="t68" role="presentation" cellpadding="0" cellspacing="0" align="center" valign="top">
-<tr class="t67"><td></td><td class="t66" width="600" valign="top">
-<table role="presentation" width="100%" cellpadding="0" cellspacing="0" class="t65" style="width:100%;"><tr><td class="t64" style="padding:0 50px 0 50px;"><table role="presentation" width="100%" cellpadding="0" cellspacing="0" style="width:100% !important;"><tr><td align="center">
-<table class="t55" role="presentation" cellpadding="0" cellspacing="0" style="Margin-left:auto;Margin-right:auto;"><tr><td width="500" class="t54" style="width:600px;">
-<table class="t53" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t52"><p class="t51" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:22px;font-weight:500;font-style:normal;font-size:12px;text-decoration:none;text-transform:none;direction:ltr;color:#888888;text-align:center;mso-line-height-rule:exactly;mso-text-raise:3px;">© <span class="t50" style="margin:0;Margin:0;mso-line-height-rule:exactly;">${new Date().getFullYear()} </span>TrailerBase. All Rights Reserved<br/></p></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td><div class="t59" style="mso-line-height-rule:exactly;mso-line-height-alt:20px;line-height:20px;font-size:1px;display:block;">&nbsp;&nbsp;</div></td></tr><tr><td align="center">
-<table class="t63" role="presentation" cellpadding="0" cellspacing="0" style="Margin-left:auto;Margin-right:auto;"><tr><td width="500" class="t62" style="width:600px;">
-<table class="t61" role="presentation" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tr><td class="t60"><p class="t58" style="margin:0;Margin:0;font-family:Albert Sans,BlinkMacSystemFont,Segoe UI,Helvetica Neue,Arial,sans-serif;line-height:22px;font-weight:500;font-style:normal;font-size:12px;text-decoration:none;text-transform:none;direction:ltr;color:#888888;text-align:center;mso-line-height-rule:exactly;mso-text-raise:3px;"><a class="t57" href="mailto:support@trailerbase.tech" style="margin:0;Margin:0;font-weight:500;font-style:normal;text-decoration:none;direction:ltr;color:#403E3E;mso-line-height-rule:exactly;" target="_blank">Support <span class="t56" style="margin:0;Margin:0;font-weight:900;text-decoration:none;color:#415AE8;mso-line-height-rule:exactly;">sakhile@trailerbase.tech</span><br/></a></p></td></tr></table>
-</td></tr></table>
-</td></tr></table></td></tr></table>
-</td>
-<td></td></tr>
-</table></div></div></td></tr></table>
-</td></tr></table>
-</td></tr><tr><td><div class="t75" style="mso-line-height-rule:exactly;mso-line-height-alt:50px;line-height:50px;font-size:1px;display:block;">&nbsp;&nbsp;</div></td></tr></table></td></tr></table></div><div class="gmail-fix" style="display: none; white-space: nowrap; font: 15px courier; line-height: 0;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div></body>
+        <!doctype html>
+<html>
+  <body>
+    <div
+      style='background-color:#ffffff;color:#FFFFFF;font-family:"Iowan Old Style", "Palatino Linotype", "URW Palladio L", P052, serif;font-size:16px;font-weight:400;letter-spacing:0.15008px;line-height:1.5;margin:0;padding:32px 0;min-height:100%;width:100%'
+    >
+      <table
+        align="center"
+        width="100%"
+        style="margin:0 auto;max-width:600px;background-color:#ffffff"
+        role="presentation"
+        cellspacing="0"
+        cellpadding="0"
+        border="0"
+      >
+        <tbody>
+          <tr style="width:100%">
+            <td>
+              <div style="padding:24px 24px 24px 24px;text-align:center">
+                <img
+                  alt=""
+                  src="https://www.trailerbase.tech/favicon.ico"
+                  style="outline:none;border:none;text-decoration:none;vertical-align:middle;display:inline-block;max-width:100%"
+                />
+              </div>
+              <div
+                style='color:#000000;font-size:16px;font-family:"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif;font-weight:normal;text-align:center;padding:16px 24px 16px 24px'
+              >
+                Welcome to TrailerBase
+              </div>
+              <h3
+                style='color:#000000;font-weight:bold;text-align:center;margin:0;font-family:"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif;font-size:20px;padding:16px 24px 16px 24px'
+              >
+                ${to}
+              </h3>
+              <div
+                style='color:#868686;font-size:16px;font-family:"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif;font-weight:normal;text-align:center;padding:16px 24px 16px 24px'
+              >
+                Trailer Base is a movie and TV trailer platform that provides
+                users with the latest trailers and information about their
+                favorite movies and TV shows. Now that you have created an
+                account, when you login, you will have an ability to bookmark
+                any movie or series you like. Enjoy and start bookmarking!
+              </div>
+              <div
+                style='color:#868686;font-size:14px;font-family:"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif;font-weight:normal;text-align:center;padding:16px 24px 16px 24px'
+              >
+                This is an automated email, please do not respond to it
+              </div>
+              <div
+                style='color:#737373;font-size:12px;font-family:"Helvetica Neue", "Arial Nova", "Nimbus Sans", Arial, sans-serif;font-weight:normal;text-align:center;padding:16px 24px 16px 24px'
+              >
+                Support? Email support@trailerbase.tech
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
 </html>`,
         text: `Welcome to TrailerBase ${userName}! We're pleased to have you on board. 
 
